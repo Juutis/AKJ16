a47b597c3b1baae82a392197e2e48f8c60d0d0aa
Juutis <>
2022-09-10 15:24:45 +0300

    Stuff

--- Islands/Assets/Scripts/Props/Cannon.cs
+++ Islands/Assets/Scripts/Props/Cannon.cs
@@ -7,8 +7,11 @@ public class Cannon : MonoBehaviour
     public float Pitch;
 
     public Transform PitchBone;
+    public Transform LeftWheel;
+    public Transform RightWheel;
 
     private Quaternion initialRotation;
+    private float wheelRotationScale = 2.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +22,22 @@ public class Cannon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        PitchBone.localRotation = initialRotation * Quaternion.AngleAxis(-Pitch, transform.right);
+        transform.Rotate(Vector3.up, -10 * Time.deltaTime);
+        RotateLeft(10 * Time.deltaTime);
+
+        PitchBone.localRotation = initialRotation * Quaternion.AngleAxis(-Pitch, Vector3.right);
     }
+
+    public void RotateLeft(float angle) {
+        LeftWheel.Rotate(Vector3.up, angle * wheelRotationScale);
+        RightWheel.Rotate(Vector3.up, angle * wheelRotationScale);
+
+    }
+
+    public void RotateRight(float angle) {
+        LeftWheel.Rotate(Vector3.up, -angle * wheelRotationScale);
+        RightWheel.Rotate(Vector3.up, -angle * wheelRotationScale);
+    }
+
+
 }
