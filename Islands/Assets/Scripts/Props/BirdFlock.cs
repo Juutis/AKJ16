b0e9480a122b5d3d1cc011d24af83ba7ae3a5fd1
Juutis <>
2022-09-11 17:49:32 +0300

    Levels

---
+++ Islands/Assets/Scripts/Props/BirdFlock.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdFlock : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 90;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var anims = GetComponentsInChildren<Animator>();
+        foreach (var anim in anims) {
+            anim.Play(0, -1, Random.value);
+        }
+        var birds = GetComponentsInChildren<Bird>();
+        foreach (var bird in birds) {
+            bird.enabled = false;
+            bird.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            bird.GetComponent<Rigidbody>().isKinematic = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.down, speed * Time.deltaTime);
+    }
+}
